79818e7cc56b5d108947de6f266234a32676ce06
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-10 16:38:41 +0800

    [evacheng]feat: completed practice 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    const {data, column} = input;
+    const result = [];
+    for (const person of data) {
+      let entity = {};
+      let index = 0;
+      for (const key of column) {
+        entity[key.name] = person[index];
+        index++;
+      }
+      result.push(entity);
+    }
+  
+    return result;
+  }
+  export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (items, sections) => {
+  let result = [];
+  for (const section of sections) {
+    let content = section.content;
+    let index = section.index;
+    let extra = sections.indexOf(section);
+    items.splice(index + extra, 0, content); 
+  }
+  result = [...items];
+  return result;
+}
+
 export { inject };
